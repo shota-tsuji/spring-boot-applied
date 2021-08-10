0f64d3875d29c8e304f30958ef634fb5ed9bc892
shota-tsuji <shotatuzi@gmail.com>
2021-08-11 06:10:30 +0900

    feat: Spring Initializarでプロジェクトを作成

---
+++ src/main/java/com/example/springbootapplied/SpringBootAppliedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapplied;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppliedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppliedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootapplied/SpringBootAppliedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapplied;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppliedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
